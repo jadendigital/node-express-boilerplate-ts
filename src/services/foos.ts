bcb01ebf669c36dbc8511220e9e8f3ddc670a73a
Aaron HS <asiraky@gmail.com>
2023-10-10 11:37:54 +1000

    added async-mutex

--- src/services/foos.ts
+++ src/services/foos.ts
@@ -1,7 +1,19 @@
+import { Mutex } from 'async-mutex'
+
 import { Foo } from '../types/Foo'
 import { knex } from '../util/knex'
 
+const mutex = new Mutex()
+
 export async function getFooNames() {
-    const foos = await knex<Foo>('foos').select()
-    return foos.map((foo) => foo.name)
+    // this is not protecting race conditions that could lead to bugs, its just an optimization
+    // strategy to prevent multiple queries from being sent to the database at the same time, given
+    // the data is user agnostic and is immediately cached in memory. we want the second caller to
+    // hit cache instead of the database.
+
+    const dbResults = await mutex.runExclusive(async () => {
+        return knex<Foo>('foos').select()
+    })
+
+    return dbResults.map((foo) => foo.name)
 }
